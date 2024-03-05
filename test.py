e5ef8abeb219836abf863a0cefebeb3f496612e4
TheoMav07 <theomav63@gmail.com>
2024-03-05 11:03:40 +0200

    Finally

--- test.py
+++ test.py
@@ -6,16 +6,18 @@ import scipy.integrate as integrate
 def f(x):
     a = 1.2
     a = (((((((-4.46785*pow(10, 7) *(x-0.35)-989101) * (x-0.452028)-37580.9) * (x-0.0287786)+8202.96) * (x-0.2)+8588.79) * (x-0.40718)-2603.65) * (x-0.0897433)+1024.68) *(x-0.299966)-45.8766) *(x-0.5) *x
-    if x >= 0.5 or x <= 0:
+    if x > 0.5 or x < 0:
         return 0.0
     else:
         return a
 
 def fd(v):
-    c = 0.0
-    c = (1.20473 * 0.00272415 * 0.5783 * np.sqrt(v)) / 2
+    c = 5.2
+    c = (1.20473 * 0.00272415 * 0.5783 * np.square(v)) / 2
     return c
 
+def avg(x):
+    return sum(x) / len(x)
 # x = np.linspace(0, 0.5, 500)
 # y = f(x)
 
@@ -32,20 +34,23 @@ vStart = 0.0
 vEnd = 0.0
 J = 0.0
 newJ = []
-m = 0.055
+m = 0.06
+vAvg = [0.0]
 
-J = integrate.quad(f, 0, 0.5)
+# J = integrate.quad(f, 0, 0.5)
 
-for i in range(1, 1000):
+for i in range(1, 3000):
     fThrust.append(f((0.0005*i)-0.0005))
     J = integrate.quad(f, (0.0005*i)-0.0005, 0.0005*i)
     vEnd = (J[0]/m) + vStart
     vStart = vEnd
-    # v.append(vEnd)
+    v.append(vEnd)
     fDrag.append(fd(vEnd))
 fThrust.append(f(0.5))
+vAvg.append(avg(v))
 vEnd = 0.0
 vStart = 0.0
+v = []
 
 # print(fThrust)
 
@@ -55,29 +60,32 @@ vStart = 0.0
 # plt.plot(x, y)
 # plt.show()
 
-for i in range(0, 999):
+for i in range(0, 3*999):
     newJ.append((fThrust[i] - fDrag[i]) * 0.0005)
     vEnd = (newJ[i]/m) + vStart
     vStart = vEnd
     fDrag[i] = fd(vEnd)
+    v.append(vEnd)
+vAvg.append(avg(v))
 vEnd = 0.0
 vStart = 0.0
 
-for k in range(1, 100):
-    v = [0.0]
-    for i in range(0, 999):
-        newJ.append((fThrust[i] - fDrag[i]) * 0.0005)
+for k in range(1, 30):
+    for i in range(0, 3*999):
+        newJ[i] = (fThrust[i] - fDrag[i]) * 0.0005
         vEnd = (newJ[i]/m) + vStart
         vStart = vEnd
-        v.append(vEnd)
+        v[i] = vEnd
         fDrag[i] = fd(vEnd)
+    vAvg.append(avg(v))
     vEnd = 0.0
     vStart = 0.0
 
 
 y = np.array(v)
-x = np.arange(float(len(y))) * 0.0005
+x = np.arange(float(len(y))) *0.0005
 
 plt.plot(x, y)
 plt.show()
 
+print(v[len(v) - 1])
